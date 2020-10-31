6dc0bf8e27a9e01af4e235c05ba4fa3b1c08aa55
vvladimir0201 <73730842+vvladimir0201@users.noreply.github.com>
2020-10-31 10:27:02 +0500

    Add files via upload

---
+++ Lab1-1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+
+    class Tree
+    {
+        private int value;
+        private Tree left;
+        private Tree right;
+
+       
+        public void Insert(int value)
+        {
+            if (this.value == 0)
+                this.value = value;
+            else
+            {
+                if (this.value.CompareTo(value) > 0)
+                {
+                    if (left == null)
+                        this.left = new Tree();
+                    left.Insert(value);
+                }
+                else if (this.value.CompareTo(value) < 0)
+                {
+                    if (right == null)
+                        this.right = new Tree();
+                    right.Insert(value);
+                }
+                
+            }
+        }
+
+
+        public string Print(Tree t)
+        {
+            string result = "";
+            if (t.left != null)
+                result += Print(t.left);
+
+            result += t.value + "\n";
+
+            if (t.right != null)
+                result += Print(t.right);
+
+            return result;
+        }
+    }   
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter f = new StreamWriter("test.txt", true);
+            Tree t = new Tree();
+            int[] arr = new int[] {72, 5, 78, 45, 34, 17, 9, 0, -2, -76 };
+
+            for (int i=0; i<arr.Length; i++)
+                t.Insert(arr[i]);
+
+            f.WriteLine(t.Print(t));
+            f.Close();
+        }
+    }
+}
---
+++ Lab1-2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            int[] dan = { 7, 60, 15, 44, 31 };
+
+            int l = 1, k = 0;
+            for (int j=0; j<= dan.Length; j++)
+            {
+                l += 2 * j;
+            }
+
+
+            string[] tree = new string[l];
+            tree[0] = Convert.ToString(dan[0]);
+            tree[1] = "f";
+            tree[2] = "f";
+
+            int  x, s;
+            bool r = false;
+            for (int i=1; i<dan.Length; i++)
+            {
+
+                s = 0;
+                r = false;
+
+                if (i == dan.Length - 1)
+                {
+                   
+
+                }
+                while (r == false)
+                {
+                    x = Convert.ToInt32(tree[s]);
+                    if (dan[i]==x)
+                        break;
+                    if ((tree[s * 2 + 1] == "f") & (dan[i] < x))
+                    {
+                        tree[2 * s + 1] = Convert.ToString(dan[i]);
+                        if (i != dan.Length - 1)
+                        {
+                            tree[(2 * s + 1) * 2 + 1] = "f";
+                            tree[(2 * s + 1) * 2 + 2] = "f";
+                        }
+                        break;
+                    }
+
+                    if ((tree[s * 2 + 2] == "f") & (dan[i] > x))
+                    {
+                        tree[2 * s + 2] = Convert.ToString(dan[i]);
+                        if (i != dan.Length - 1)
+                        {
+                            tree[(2 * s + 2) * 2 + 1] = "f";
+                            tree[(2 * s + 2) * 2 + 2] = "f";
+                        }
+                        break;
+                    }
+                    if ((tree[s * 2 + 1] != "f") & (dan[i] < x))
+                        s = s * 2 + 1;
+                    if ((tree[s * 2 + 2] != "f") & (dan[i] > x))
+                        s = s * 2 + 2;
+                }
+                
+               
+                    
+            
+            }
+
+            
+
+
+
+
+            StreamWriter n = new StreamWriter("test2.txt", true);
+
+            for (int i=0; i<l; i++)
+                if (tree[i] != "f")
+                    n.WriteLine(tree[i]);
+
+
+
+
+
+            n.Close();
+        }
+    }
+}
